ed89afe910039e64718489bef67ad45d0ee6acf9
buooo6 <mattyh383@gmail.com>
2016-11-11 10:22:02 +0000

    Login form with user authentication

---
+++ LogInForm/dbo.Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserID]   INT        IDENTITY (1, 1) NOT NULL,
+    [Username] NCHAR (10) NOT NULL,
+    [Password] NCHAR (10) NOT NULL,
+    [UserRole] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([UserID] ASC), 
+    CONSTRAINT [FK_Users_ToTable] FOREIGN KEY ([UserRole]) REFERENCES [UserRoles]([RoleID])
+);
+
