0c6c69e43e88f4ff42ffe7ed4019e048f0553c84
Will1036 <william.boyer1234@hotmail.com>
2016-11-13 17:23:27 +0000

    ResponseComboBox and TextBox now interacting with new Database Connection

---
+++ SoftEngProject/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftEngProject
+{
+    class Constants
+    {
+        public static String sqlQuerySelectAll = "SELECT * FROM Templates";
+        public static String sqlQuerySelResponses = "SELECT ResponseName FROM Responses";
+    }
+}
---
+++ SoftEngProject/Response.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SoftEngProject
+{
+    public class Response
+    {
+        private int _id;
+        private string _responseName;
+        private string _groupID;
+        private string _message;
+
+        public Response(int id, string responseName, string message, string groupID)
+        {
+            this._id = id;
+            this._responseName = responseName;
+            this._message = message;
+            this._groupID = groupID;
+        }
+
+        public int ID { get { return _id; } }
+        public string ResponseName { get { return _responseName; } }
+        public string Message { get { return _message; } }
+        public string GroupID { get { return _groupID; } }
+    }
+}
